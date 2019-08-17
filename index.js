a34dbe02af523623756b93c62848dcf0f5932fe1
ghehd231 <ghehd231@naver.com>
2019-08-17 12:08:13 +0900

    app js

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-import express from "express";
-// const express = require("express");
-import morgan from "morgan";
-import helmet from "helmet";
-import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
-
-const app = express();
+import app from "./app";
 
 
 
 const handleListening = () => {
     console.log(`Listening on: http://localhost:${PORT}`);
 }
-const handleHome = (req, res) => res.send("Hello from my ass");
-const handleProfile = (req, res) => res.send("You are on my profile");
-
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
-app.use(morgan("dev"));
-
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-
-
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, handleListening);
